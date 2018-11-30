8ba42cda09e8fb5c1db075a3efeb5e3011396d51
garryck-davies <garryck.b.davies@gmail.com>
2018-11-30 16:40:27 -0700

    another commit

--- src/components/Mens/Mens.js
+++ src/components/Mens/Mens.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import axios from 'axios';
 
 
 
@@ -13,15 +13,34 @@ export default class Mens extends Component {
         }
     }
 
-    componentDidMount(){
-        
+   async componentDidMount(){
+        let res = await axios.get('/api/products')
+        console.log(res)    
     }
+
+    handleAddToBag(item) {
+        this.props.updateCart(item)
+    }
+    
+
     render() {
         
 
         return(
             <div className="mens-container box-container">
-                Mens
+              <p>Men's</p>
+              <div className="mens-all-item-component">
+                {this.state.products.map((product, i) => {
+                    return (
+                        <div className='mens-item-component' key={i}>
+                            <img className="mens-product-image" src={product.item_img} alt="img" />
+                            <p className="item-name" src={product.item_name} />
+                            <p>{product.price}</p>
+                            <button onClick={() => this.handleAddToBag()}>Add To Bag</button>
+                        </div>
+                    )
+                })}
+              </div>
             </div>
         )
     }
